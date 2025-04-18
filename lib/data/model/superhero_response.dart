a5ea00b7b7ff435d00590ccacd1648ca195c2abb
Alfredo Aguilar <jose.aguilarlara1999@gmail.com>
2025-04-18 00:53:15 -0600

    Refactor SuperheroResponse y SuperheroSearchScreen para manejar detalles de superhéroes y mejorar la visualización

--- lib/data/model/superhero_response.dart
+++ lib/data/model/superhero_response.dart
@@ -1,9 +1,25 @@
+import 'package:superhero_app/data/model/superhero_response_detail.dart';
+
 class SuperheroResponse {
   final String response;
+  final List<SuperheroResponseDetail> reponseSuperheroDetail;
 
-  SuperheroResponse({required this.response});
+  SuperheroResponse({
+    required this.response,
+    required this.reponseSuperheroDetail,
+  });
 
   factory SuperheroResponse.fromJson(Map<String, dynamic> json) {
-    return SuperheroResponse(response: json["response"]);
+    var list = json["results"] as List?;
+    List<SuperheroResponseDetail> heroList =
+        list != null
+            ? list
+                .map((hero) => SuperheroResponseDetail.fronJson(hero))
+                .toList()
+            : [];
+    return SuperheroResponse(
+      response: json["response"] ?? "error",
+      reponseSuperheroDetail: heroList,
+    );
   }
 }
--- lib/screens/superhero_search_screen.dart
+++ lib/screens/superhero_search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:superhero_app/data/model/superhero_response.dart';
+import 'package:superhero_app/data/model/superhero_response_detail.dart';
 import 'package:superhero_app/data/repository.dart';
 
 class SuperheroSearchScreen extends StatefulWidget {
@@ -33,22 +34,85 @@ class _SuperheroSearchScreenState extends State<SuperheroSearchScreen> {
               },
             ),
           ),
-          FutureBuilder(
-            future: _superheroInfo,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text("Error: ${snapshot.error}");
-              } else if (snapshot.hasData) {
-                return Text("${snapshot.data?.response}");
-              } else {
-                return Text("No hay resultados");
-              }
-            },
-          ),
+          bodyList(),
         ],
       ),
     );
   }
+
+  FutureBuilder<SuperheroResponse?> bodyList() {
+    return FutureBuilder(
+      future: _superheroInfo,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text("Error: ${snapshot.error}");
+        } else if (snapshot.hasData) {
+          var superheroList = snapshot.data?.reponseSuperheroDetail;
+          if (superheroList == null || superheroList.isEmpty) {
+            return Text("No hay resultados");
+          }
+          return Expanded(
+            child: ListView.builder(
+              itemCount: superheroList.length,
+              itemBuilder: (context, index) {
+                return itemSuperhero(superheroList[index]);
+              },
+            ),
+          );
+        } else {
+          return Text("No hay resultados");
+        }
+      },
+    );
+  }
+
+  Padding itemSuperhero(SuperheroResponseDetail hero) => Padding(
+    padding: const EdgeInsets.only(top: 8, bottom: 8, left: 16, right: 16),
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.redAccent,
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.network(
+              hero.url,
+              height: 250,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              alignment: Alignment(0, -0.6),
+              errorBuilder: (context, error, stackTrace) {
+                // Mostrar un widget alternativo si ocurre un error
+                return Container(
+                  height: 250,
+                  width: double.infinity,
+                  color: Colors.grey,
+                  child: Icon(
+                    Icons.broken_image,
+                    color: Colors.white,
+                    size: 50,
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              hero.name,
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
